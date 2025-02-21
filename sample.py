68fe188afdb4b79043a24f45b453afef4eb19c7d
MithuniKarunathilake <mithunisk03@gmail.com>
2025-02-21 10:44:54 +0530

    test:checking a sample file

--- sample.py
+++ sample.py
@@ -3,3 +3,19 @@ import requests
 import os
 
 app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    action = data.get('action')
+    repo_name = data['repository']['full_name']
+    pr_number = data['pull_request']['number']
+    pr_title = data['pull_request']['title']
+
+    message = f'PR #{pr_number}: {pr_title} ({action}) in {repo_name}'
+
+    webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+    if webhook_url:
+        requests.post(webhook_url, json={'content': message})
+
+    return '', 200
